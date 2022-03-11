6b2b7a1af8e333e67b0d0b45fb89b80877bd0b5e
Wander Rodrigues <wander.rodrigues@writ.solutions>
2022-03-11 20:49:16 +0000

    Start project

---
+++ app/src/main/java/com/falou/avaliacao_tecnica_falou/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.falou.avaliacao_tecnica_falou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
